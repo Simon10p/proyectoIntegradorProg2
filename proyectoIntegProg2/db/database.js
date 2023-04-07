d3c21cb7628d56ef685c1675dd4186bcff33bfe0
nicoshakalis <nshakalis@udesa.edu.ar>
2023-04-07 13:03:59 -0300

    avance base de datos remeras

--- proyectoIntegProg2/db/database.js
+++ proyectoIntegProg2/db/database.js
@@ -1,21 +1,93 @@
 let data = {
     remeras: [
         {
-            nombre: '2',
-            imgUrl: '2',
-            descripcion: '2',
+            nombre: 'Argentina 1986',
+            imgUrl: '/images/argentina-1986-1-1.jpeg',
+            descripcion: 'Camiseta vintage de fútbol de Argentina, ganador de la Copa del Mundo 1986 en México, Argentina-Alemania 3-2',
             comentarios: [
-                {}
+                {MateoPodetti: 'Muy buena la remera, es la original que se uso. Me trae muy buenos recuerdos'}
             ]
-        }
+        },
+        {
+            nombre: 'Barcelona 1973-74',
+            imgUrl: '/images/barcelona-1973-74-1.jpeg',
+            descripcion: 'Camiseta vintage de fútbol del Barcelona 1973/74. Ganador de la Liga, campeón de España',
+            comentarios: [
+                {Nicoshakalis: 'La calidad es la de la original, no me lo esperaba' }
+            ]
+        },
+        {
+            nombre: 'Arabia Saudita 1998',
+            imgUrl: '/images/saudi-arabia-1998-1.jpeg',
+            descripcion: 'Camiseta vintage de fútbol de Arabia Saudita. Copa del Mundo 1998 en Francia',
+            comentarios: [
+                { SimonDP: 'Es la original, no la conseguia en ningun lado y aca la encontre'}
+            ]
+        },
+        {
+            nombre: 'Atletico Madrid 1985-86',
+            imgUrl: '/images/atletico-madrid-1985-86-1.jpeg',
+            descripcion: 'Camiseta vintage de fútbol del Atlético Madrid 1985/86. Ganador de la Supercopa de España, Atlético Madrid-Barcelona 3-1',
+            comentarios: [
+                { Usuario118: ' Tremenda!'}
+            ]
+        },
+        {
+            nombre: 'Austria 1938',
+            imgUrl: '/images/Austira-1938.jpeg',
+            descripcion: 'Camiseta vintage de fútbol de Austria 1938, segunda equipación. Camiseta usada en el partido del Anschluss entre Austria y Alemania para celebrar la anexión a este última que tenía que terminar con un empate pero Austria ganó gracias a las hazañas de Matthias Sindelar',
+            comentarios: [
+                { }
+            ]
+        },
+        {
+            nombre: 'Botafogo 1968',
+            imgUrl: '/images/botafogo-1968-1.jpeg',
+            descripcion: 'Camiseta vintage de fútbol del Botafogo 1968. Ganador de la Taça Brasil, Botafogo-Fortaleza 2-2 e 4-0. Ganador del Campeonato Carioca, Botafogo-Vasco da Gama 4-0',
+            comentarios: [
+                { }
+            ]
+        },
+        {
+            nombre: 'Birmingham City 1972-73',
+            imgUrl: '/images/birmingham-city-1972-73-1.jpeg',
+            descripcion: 'Camiseta vintage de fútbol del Birmingham City 1972/73',
+            comentarios: [
+                { }
+            ]
+        },
+        {
+            nombre: 'Arsenal 1985-86',
+            imgUrl: '/images/arsenal-1985-86-1.jpeg',
+            descripcion: 'Camiseta vintage de fútbol del Arsenal 1985/86',
+            comentarios: [
+                { }
+            ]
+        },
+        {
+            nombre: 'Checoslovaquia 1976',
+            imgUrl: '/images/ceskoslovensko-1976-1.jpeg',
+            descripcion: 'Camiseta vintage de fútbol della Cecoslovacchia. Ganador de la Eurocopa 1976 en Yugoslavia, Checoslovaquia-Alemania 5-3 (penaltis)',
+            comentarios: [
+                { }
+            ]
+        },
+        {
+            nombre: 'Juventus 1984-85',
+            imgUrl: '/images/juventus-1984-85-10.jpeg',
+            descripcion: 'Camiseta vintage de fútbol de la Juventus 1984/85. Ganador de la Copa de Europa, Juventus-Liverpool 1-0',
+            comentarios: [
+                { }
+            ]
+        },
 
     ],
     usuario: {
-        email: "",
-        usuario: "",
-        contraseña: "",
-        fechaNacimiento: "",
-        DNI: "",
+        email: "Nicoshakalis@gmail.com",
+        usuario: "Nicoshakalis",
+        contraseña: "hola1234",
+        fechaNacimiento: "06/03/2002",
+        DNI: "46834788",
         pf: ""
     },
     comentarios: [
