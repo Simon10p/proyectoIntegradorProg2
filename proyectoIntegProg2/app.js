7664c090e857314514e4493c670a254b5e5c183a
Simon10p <sdiezpena@udesa.edu.ar>
2023-06-05 17:56:14 -0300

    correccion de errores

--- proyectoIntegProg2/app.js
+++ proyectoIntegProg2/app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app,use(session({
+app.use(session({
   secret:'----',
   resave: false,
   saveUninitialized: false
